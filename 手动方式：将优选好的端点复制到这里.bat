6a0ec47a0b9487caa4d90918bd3e65eccffa9fe3
Juerson <37030166+juerson@users.noreply.github.com>
2025-05-30 11:42:01 +0800

    update

--- 手动方式：将优选好的端点复制到这里.bat
+++ 手动方式：将优选好的端点复制到这里.bat
@@ -10,11 +10,11 @@ set /p choice="输入你的选择(1或2)："
 
 rem 选择隧道协议
 if "!choice!"=="1" (
-    set "tunnelProtocol=MASQUE"
-) else if "!choice!"=="2" (
-    set "tunnelProtocol=WireGuard"
-) else (
-    goto loop
+  set "tunnelProtocol=MASQUE"
+  ) else if "!choice!"=="2" (
+  set "tunnelProtocol=WireGuard"
+  ) else (
+  goto loop
 )
 
 rem 设置隧道协议
@@ -27,17 +27,17 @@ rem 进入死循环，捕获用户输入的endpoint
 :endpointLoop
 set /p endpoint="请输入Endpoint的值(输入'exit'退出)："
 if /i "!endpoint!"=="exit" (
-    echo 退出程序。
-    exit /b
+  echo 退出程序。
+  exit /b
 )
 
 if defined endpoint (
-	warp-cli disconnect
-	warp-cli tunnel endpoint reset
-	warp-cli tunnel endpoint set %endpoint%
-	warp-cli connect
-	cls rem 清空控制台内容
-	echo $ warp-cli tunnel protocol set %tunnelProtocol%
-	echo $ warp-cli tunnel endpoint set %endpoint%
+  warp-cli disconnect
+  warp-cli tunnel endpoint reset
+  warp-cli tunnel endpoint set %endpoint%
+  warp-cli connect
+  cls rem 清空控制台内容
+  echo $ warp-cli tunnel protocol set %tunnelProtocol%
+  echo $ warp-cli tunnel endpoint set %endpoint%
 )
 goto endpointLoop
